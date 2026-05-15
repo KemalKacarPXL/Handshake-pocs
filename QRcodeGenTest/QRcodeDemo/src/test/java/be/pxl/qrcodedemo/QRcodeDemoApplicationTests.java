d34e5180f7a4bca50e411bc46a78db22062009f3
Kemal Kacar <kemal.kacar@student.pxl.be>
2026-05-15 20:24:26 +0200

    QR code generatie

---
+++ QRcodeGenTest/QRcodeDemo/src/test/java/be/pxl/qrcodedemo/QRcodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.qrcodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QRcodeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
